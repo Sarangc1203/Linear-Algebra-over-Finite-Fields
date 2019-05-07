3895c91f1ecb06161d1ba207c7b369c8445e0bad
Aryan187 <45520218+Aryan187@users.noreply.github.com>
2019-05-07 12:17:41 +0530

    Update Block Lanczos.cpp
    Corrected the code

--- Block Lanczos/Block Lanczos.cpp
+++ Block Lanczos/Block Lanczos.cpp
@@ -46,7 +46,7 @@ int i,j;
 int sum=0,n=v.size();
 for (i=0;i<n;i++){
 	for (j=0;j<n;j++){
-	sum=sum+v[i]*A[j][i]*w[i];
+	sum=sum+v[j]*A[j][i]*w[i];
 	sum=sum%m;}
 	}
 	return sum;
@@ -61,7 +61,7 @@ int n=v.size();
 for (i=0;i<n;i++){
 	result.push_back(0);
 	for (j=0;j<n;j++){
-	result[i]=result[i]+A[i][j]*v[i];
+	result[i]=result[i]+A[i][j]*v[j];
 	result[i]=result[i]%m;}
 	}
 	return result;
@@ -70,12 +70,11 @@ for (i=0;i<n;i++){
 // to get v(t).w
 
 int multiply3(vector<int> v,vector<int> w,int m) {
-int i,j;
+int i;
 int sum=0,n=v.size();
 for (i=0;i<n;i++){
-	for (j=0;j<n;j++){
 	sum=sum+v[i]*w[i];
-	sum=sum%m;}
+	sum=sum%m;
 	}
 	return sum;
 }
@@ -113,26 +112,31 @@ int main() {
 	cout<<b[i]<<" ";}
 	cout<<endl;
 	
-	
 	auto start = high_resolution_clock::now();
 	
 	vector<vector<int>> V;
 	V.push_back(b);
-	
+	auto start2 = high_resolution_clock::now();
 	vector<vector<int>> sqrA= square(A,m,n);
+	auto stop2 = high_resolution_clock::now();
+	auto duration2 = duration_cast<microseconds>(stop2 - start2);
+
+	cout << "Time:"<<duration2.count()<<endl;
 	int l;
 	for (i=1;;i++){
 		vector <int> temp;
-		vector <int> temp2= multiply2(A,getRow(V,i-1,n),m);
-		int c1= multiply(temp2,sqrA,temp2,m)*modArthInv(multiply(temp2,A,temp2,m),m)%m;
+		vector <int> temp2= multiply2(A,V[i-1],m);
+		vector <int> temp3= V[i-1];
+		int c1= multiply(temp3,sqrA,temp3,m)*modArthInv(multiply(temp3,A,temp3,m),m)%m;
 		int c2;
 		if (i!=1)
-		c2=multiply(temp2,A,temp2,m)*modArthInv(multiply(getRow(V,i-2,n),A,getRow(V,i-2,n),m),m)%m;
+		c2=multiply(temp3,A,temp3,m)*modArthInv(multiply(V[i-2],A,V[i-2],m),m)%m;
 		for (j=0;j<n;j++){
 			if (i!=1)
-			temp.push_back((temp2[j]+m-c1*getRow(V,i-1,n)[j]%m+m-c2*getRow(V,i-2,n)[j]%m)%m);
+			temp.push_back((temp2[j]+m-c1*V[i-1][j]%m+m-c2*V[i-2][j]%m+m)%m);
 			else
-			temp.push_back((temp2[j]+m-c1*getRow(V,i-1,n)[j]%m)%m);}
+			temp.push_back((temp2[j]+m-c1*V[i-1][j]%m)%m);
+		}
 		V.push_back(temp);
 		if (multiply(temp,A,temp,m)==0){
 		l=i;
@@ -144,7 +148,7 @@ int main() {
 	for (i=0;i<n;i++){
 	    x.push_back(0);}
 	for (i=0;i<l;i++){
-		vector<int> temp= getRow(V,i,n);
+		vector<int> temp= V[i];
 		int c=(multiply3(temp,b,m)*modArthInv(multiply(temp,A,temp,m),m))%m;
 		for (j=0;j<n;j++){
 			x[j]=x[j]+(temp[j]*c);
