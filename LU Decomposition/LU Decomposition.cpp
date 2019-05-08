89d8cbe42713ff9aa9404b0129977e69ed2cf153
Sarang Chaudhari <43308378+Sarangc1203@users.noreply.github.com>
2019-05-08 12:23:01 +0530

    Update LU Decomposition.cpp

--- LU Decomposition/LU Decomposition.cpp
+++ LU Decomposition/LU Decomposition.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <vector>
+#include <chrono>
+#include<time.h>
 using namespace std;
+using namespace std::chrono;
 
 int power(int x,int n,int m){
     if (n==0)
@@ -18,15 +22,17 @@ int modArthInv(int a,int m){
 
 int main()
 {
-    int n = 100, m=3, t;
+    int n = 10, m=11, t;
+    int ttt = 100;
+    int ns = 0;
 	//cin >> n >> m;
-	vector< vector<int> > mat;
-	int L[n][n];
+	vector< vector<int> > mat, L;
+	vector<int> B;
 	srand(time(0)+ttt*124);
 	int i, j, k;
 	for (i = 0; i < n; i++)
 	{
-		vector<int> temp;
+		vector<int> temp, temp1;
 		for (j = 0; j <= n; j++)
 		{
 			//cin >> t;
@@ -35,10 +41,17 @@ int main()
                 //t = 0;
 			//cout << t << " ";
 			temp.push_back(t);
-			L[i][j] = (i==j)?1:0;
+			if( i == j)
+                temp1.push_back(1);
+            else
+                temp1.push_back(0);
 		}
 		//cout << "\n";
 		mat.push_back(temp);
+		L.push_back(temp1);
+		int t1;
+		t1 = rand() % m;
+		B.push_back(t1);
 	}
 	auto start = high_resolution_clock::now();
 	for (i = 0; i < n; i++) {
@@ -58,21 +71,34 @@ int main()
 			ns++;
 			break;
 		}
-	}
-	else
+		else
     {
         for(int j = i +1; j < n; j++)
         {
-            int p = (modArthInv(m, mat[i][i]) * mat[j][i]) % m
-            L[j][i1] = p
+            int p = (modArthInv(m, mat[i][i]) * mat[j][i]) % m;
+            if(p<0)
+                p+=m;
+            L[j][i] = p;
+            cout<<p<<"\t";
             for(int k = 0; k < n; k++)
             {
-                U[j][k] -= p * U[i, k];
-                U[j][k] %= m;
+                mat[j][k] -= p * mat[i][k];
+                mat[j][k] %= m;
+                if(mat[j][k]<0)
+                mat[j][k]+=m;
             }
         }
     }
-    auto stop = high_resolution_clock::now();
+	}
+	auto stop = high_resolution_clock::now();
+	cout<<"MAT:"<<endl;
+	for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+            cout<<mat[i][j]<<"\t";
+        cout<<endl;
+    }
+
 	auto duration = duration_cast<microseconds>(stop - start);
 	cout<<duration.count()<<endl;
     return 0;
